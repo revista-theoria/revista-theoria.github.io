0fdd2148b74f55847dba25313b8f5344fc849442
Victor <victorlaureanovega@gmail.com>
2025-07-20 10:56:01 -0600

    Segunda edición

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -14,6 +14,12 @@ function Contacto() {
                         Agradecemos tu interés en contactarnos.
                     </p>
                     <p>
+                        Si deseas preguntarnos algo, puedes enviarnos un correo electrónico a re.theoria@gmail.com o un mensaje a nuestro Instagram: @revista.theoria.
+                    </p>
+                </div>
+
+                <div className="descripcion">
+                    <p>
                         Si deseas colaborar con nosotros, puedes compartirnos tu trabajo aquí:
                     </p>
                 </div>
@@ -22,12 +28,6 @@ function Contacto() {
                         title="Formulario para envío de colaboraciones"
                         src="https://docs.google.com/forms/d/e/1FAIpQLSc-J1w2F4mnPzjHS7wFM8aZi-Bv8D2kpGt3CA4aBt8VoKv6Cg/viewform?embedded=true"
                     />
-                </div>
-
-                <div className="descripcion">
-                    <p>
-                        Si deseas preguntarnos algo, puedes enviarnos un correo electrónico a re.theoria@gmail.com o un mensaje a nuestro Instagram: @revista.theoria.
-                    </p>
                 </div>
             </section>
             <Footer />
