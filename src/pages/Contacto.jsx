e1fd2b5fc1211b01167fb7feea0a4028a3947ebc
Victor <victorlaureanovega@gmail.com>
2025-07-20 10:53:20 -0600

    Segunda edición

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -26,27 +26,8 @@ function Contacto() {
 
                 <div className="descripcion">
                     <p>
-                        Si deseas preguntarnos algo, puedes hacerlo a través del siguiente formulario o enviándonos un mensaje a nuestro Instagram:
+                        Si deseas preguntarnos algo, puedes enviarnos un correo electrónico a re.theoria@gmail.com o un mensaje a nuestro Instagram: @revista.theoria.
                     </p>
-                </div>
-                <div className="formulario-general">
-                    <form id="form" method="POST" action="https://formsubmit.co/re.theoria@gmail.com">
-                        <div class="input">
-                            <div>
-                                <input type="text" name="nombre" placeholder="Nombre" required />
-                            </div>
-                            <div>
-                                <input type="email" name="correo" placeholder="Correo electrónico" required />
-                            </div>
-                        </div>
-                    
-                        <textarea id="mensaje" name="mensaje" rows="5" placeholder="Mensaje" required></textarea>
-                    
-                        <button type="submit">Enviar</button>
-
-                        <input type="hidden" name="_next" value="revista-theoria.github.io/#/contacto" />
-                        <input type="hidden" name="_captcha" value="false" />
-                    </form>
                 </div>
             </section>
             <Footer />
